d4e5cda0450845a57ddb2edd13c6a72d56ed920b
Sumit Kumar <titanicsumit@gmail.com>
2021-07-08 12:11:13 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,30 @@
+window.onload = function () {
+  getQuote().then((data) => setQuotehelper(data));
+};
+
+async function getQuote() {
+  const url = "https://type.fit/api/quotes";
+  const response = await fetch(url);
+  var data = await response.json();
+  return data;
+}
+
+function setQuotehelper(data) {
+  data = data.filter((q) => q.text.length < 100);
+
+  setInterval(() => {
+    setQuote(data[Math.floor(Math.random() * data.length)]);
+  }, 3000);
+}
+
+function setQuote(q) {
+  var x = document.getElementById("quote");
+
+  x.innerHTML =
+    '"' +
+    q.text +
+    '"<br/>' +
+    '<span style="color: #00adb5">' +
+    (q.author ? q.author : "Anonymous") +
+    "</span>";
+}
